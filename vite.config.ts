ebd8247ec2654da9b287c9e9c08986725ba73094
Tom Greenwood <tomgreenwood1@gmail.com>
2025-11-14 17:12:29 +0000

    fix vite

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+// import Inspect from  'vite-plugin-inspect'
+import { defineConfig } from 'vite'
+// import { resolve } from 'path'
+
+export default  defineConfig({
+    base: '/explore-wotton/',
+})
