3d3ede9ccc74476043691722ca688a4472437eb9
Tom Greenwood <tomgreenwood1@gmail.com>
2025-11-18 16:50:17 +0000

    add 3d shape into scene

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,142 @@
-  import mapboxgl from 'mapbox-gl'
-  import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl, { Map, type CustomLayerInterface, type LngLatLike } from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
+import * as THREE from 'three'
+const LONG_STREET_LNG_LAT: LngLatLike = [-2.35104, 51.63764]
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9tZ3JlZW53b29kIiwiYSI6ImNsb2E5dThjNzBsbjkyanFxd2hiOHB0bTMifQ.OnRKHkIO_7GKC7vSvdx7og';
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoidG9tZ3JlZW53b29kIiwiYSI6ImNsb2E5dThjNzBsbjkyanFxd2hiOHB0bTMifQ.OnRKHkIO_7GKC7vSvdx7og';
-    const map = new mapboxgl.Map({
-        container: 'map',
-        antialias: true,
-        zoom: 18,
-        center: [-2.35104, 51.63764],
-        pitch: 80,
-        bearing: 280,
-        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/tomgreenwood/clymqewzs002001r12a8g3z4f',
-        config: {
-            // Initial configuration for the Mapbox Standard style set above. By default, its ID is `basemap`.
-            basemap: {
-                // Here, we're disabling all of the 3D layers such as landmarks, trees, and 3D extrusions.
-                show3dObjects: false,
-        
-            },
-        }
-    });
-
-    map.on('style.load', () => {
-        map.addSource('mapbox-dem', {
-            'type': 'raster-dem',
-            'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-            'tileSize': 512,
-            'maxzoom': 14
-        });
+const map = new mapboxgl.Map({
+    container: 'map',
+    antialias: true,
+    zoom: 18,
+    center: LONG_STREET_LNG_LAT,
+    pitch: 80,
+    bearing: 280,
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/tomgreenwood/clymqewzs002001r12a8g3z4f',
+    config: {
+        // Initial configuration for the Mapbox Standard style set above. By default, its ID is `basemap`.
+        basemap: {
+            // Here, we're disabling all of the 3D layers such as landmarks, trees, and 3D extrusions.
+            show3dObjects: false,
     
-        // add the DEM source as a terrain layer 
-        map.setTerrain({ 'source': 'mapbox-dem' });
+        },
+    }
+});
+
+map.on('style.load', () => {
+    map.addSource('mapbox-dem', {
+          'type': 'raster-dem',
+          'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+          'tileSize': 512,
+          'maxzoom': 14
     });
+
+    // add the DEM source as a terrain layer 
+    map.setTerrain({ 'source': 'mapbox-dem' });
+});
+  
+
+//////// models
+// parameters to ensure the model is georeferenced correctly on the map
+const modelOrigin: LngLatLike = LONG_STREET_LNG_LAT;
+const modelAltitude = 90;
+const modelRotate = [Math.PI / 2, 0, 0];
+const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+    modelOrigin,
+    modelAltitude
+);
+
+// transformation parameters to position, rotate and scale the 3D model onto the map
+const modelTransform = {
+    translateX: modelAsMercatorCoordinate.x,
+    translateY: modelAsMercatorCoordinate.y,
+    translateZ: modelAsMercatorCoordinate.z,
+    rotateX: modelRotate[0],
+    rotateY: modelRotate[1],
+    rotateZ: modelRotate[2],
+    /* Since the 3D model is in real world meters, a scale transform needs to be
+    * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+    */
+    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+};
+
+type ThreeEngine = {
+    camera: THREE.Camera
+    scene: THREE.Scene
+    map: Map
+    renderer: THREE.WebGLRenderer
+}
+
+// configuration of the custom layer for a 3D model per the CustomLayerInterface
+const customLayer: CustomLayerInterface & { three?: ThreeEngine } = {
+    id: '3d-model',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd: function (map: Map, gl: WebGLRenderingContext) {
+        const camera = new THREE.Camera();
+        const scene = new THREE.Scene();
+        // create two three.js lights to illuminate the model
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        scene.add(directionalLight);
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        scene.add(directionalLight2);
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        
+        const sphereGeometry = new THREE.SphereGeometry(2)
+
+        scene.add(new THREE.Mesh(sphereGeometry, new THREE.MeshLambertMaterial({color: 'red'})))
+        
+        // use the Mapbox GL JS map canvas for three.js
+        const renderer = new THREE.WebGLRenderer({
+            canvas: map.getCanvas(),
+            context: gl,
+            antialias: true
+        });
+        renderer.autoClear = false;
+        this.three = {
+            camera, scene, map, renderer
+        }
+    },
+    render: function (gl, matrix) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(1, 0, 0),
+            modelTransform.rotateX
+        );
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 1, 0),
+            modelTransform.rotateY
+        );
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 0, 1),
+            modelTransform.rotateZ
+        );
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+            .makeTranslation(
+                modelTransform.translateX,
+                modelTransform.translateY,
+                modelTransform.translateZ
+            )
+            .scale(
+                new THREE.Vector3(
+                    modelTransform.scale,
+                    -modelTransform.scale,
+                    modelTransform.scale
+                )
+            )
+            .multiply(rotationX)
+            .multiply(rotationY)
+            .multiply(rotationZ);
+        this.three!.camera.projectionMatrix = m.multiply(l);
+        this.three!.renderer.resetState();
+        this.three!.renderer.render(this.three!.scene, this.three!.camera);
+        this.three!.map.triggerRepaint();
+    }
+};
+
+map.on('style.load', () => {
+      map.addLayer(customLayer);
+});
+
